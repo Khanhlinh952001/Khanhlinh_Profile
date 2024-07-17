45fe575b45e796e9387dbc07235fe11a9af9efa9
Vo Khanh Linh <vokhanhlinh@Vos-Mac-mini.local>
2024-07-17 14:13:33 +0900

    full fix

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -4,7 +4,7 @@ function AppFooterCopyright() {
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
 				<a
-					href="https://github.com/realstoman/react-tailwindcss-portfolio"
+					href="https://github.com/Khanhlinh952001/Khanhlinh_Profile"
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
